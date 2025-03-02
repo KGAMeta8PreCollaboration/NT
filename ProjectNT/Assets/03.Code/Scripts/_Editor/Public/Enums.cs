c40cc15fe1a79611ad335dfe89980aed532e486a
DESKTOP-99CV4K8\qpalz <qpalzm60515@gmail.com>
2025-03-02 17:10:49 +0900

    [feat] 공용으로 사용할 Enums 클래스 스크립트 추가

---
+++ ProjectNT/Assets/03.Code/Scripts/_Editor/Public/Enums.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enums
+{
+    public enum Difficulty
+    {
+        EASY,
+        NORMAL,
+        HARD,
+        EXTREAM
+    }
+    public enum Gamemode
+    {
+        SOLO,
+        DUO-1,
+        DUO-2
+    }
+
+    public enum Phase
+    {
+        FIRST,
+        SECOND,
+        THIRD
+    }
+
+}
