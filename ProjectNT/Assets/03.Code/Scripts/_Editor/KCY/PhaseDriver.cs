e3cc6cd5cfaa6cd316f44704ab1d3345589f06df
KimChanYoung <qpalzm605@gmail.com>
2025-03-04 17:45:10 +0900

    [STYLE] 주석추가

--- ProjectNT/Assets/03.Code/Scripts/_Editor/KCY/PhaseDriver.cs
+++ ProjectNT/Assets/03.Code/Scripts/_Editor/KCY/PhaseDriver.cs
@@ -23,6 +23,7 @@ public class PhaseDriver : MonoBehaviour
     public Enums.ModeDiff modeDiff
     {
         get { return currentModeDiff; }
+
         set { currentModeDiff = value; }
     }
 
@@ -32,63 +33,92 @@ public class PhaseDriver : MonoBehaviour
     }
     private void AddNewPhase()
     {
+        //페이즈 10개 생성 시 추가생성 불가
         if (linkedPhase.Count == 10)
         {
             //TODO : 더이상 페이즈를 생성할 수 없습니다. 팝업 띄우기
             return;
         }
+
         GameObject newPhase =
         Instantiate(newPhasePrefab, phaseRect, false);
         ReplaceAddBTN();
         PhaseElement temp = newPhase.GetComponent<PhaseElement>();
+        //현재 난이도에 따라 페이즈 난이도 설정
         temp.modeDiff = currentModeDiff;
         linkedPhase.AddLast(temp);
-
+        //페이즈 별 번호 추가
         if (!byDifficulty.ContainsKey(currentModeDiff))
+        {
             byDifficulty.Add(currentModeDiff, linkedPhase.Count);
-        else byDifficulty[currentModeDiff] = linkedPhase.Count;
+        }
+        else
+        {
+            byDifficulty[currentModeDiff] = linkedPhase.Count;
+        }
         temp.phaseNum = byDifficulty[currentModeDiff];
+        //저장 액션 등록
         temp.action += temp.SaveAction;
+
+        //새로운 페이즈 추가시 스크롤바 Value변경
         StartCoroutine(ScrollBarCtrl());
     }
     public void SwapPhaseUp(PhaseElement other)
     {
+        //이전노드 없을시 리턴
         if (linkedPhase.Find(other).Previous == null)
         {
             Debug.Log("이전노드 없음");
             return;
         }
+
+        //변경할 노드 찾음
         LinkedListNode<PhaseElement> prevNode = linkedPhase.Find(other).Previous;
         linkedPhase.Remove(other);
         linkedPhase.AddBefore(prevNode, other);
+
+        //페이즈 번호 스왑
         int tempNum = prevNode.Value.phaseNum;
         prevNode.Value.phaseNum = other.phaseNum;
         other.phaseNum = tempNum;
+
+        //페이즈 RectTransform 정렬
         foreach (PhaseElement phase in linkedPhase)
         {
             phase.gameObject.transform.SetParent(null);
             phase.gameObject.transform.SetParent(phaseRect);
         }
+
+        //페이즈 추가버튼 정렬
         ReplaceAddBTN();
     }
     public void SwapPhaseDown(PhaseElement other)
     {
+        //다음노드 없을시 리턴
         if (linkedPhase.Find(other).Next == null)
         {
             Debug.Log("다음노드 없음");
             return;
         }
+
+        //변경할 노드 찾음
         LinkedListNode<PhaseElement> nextNode = linkedPhase.Find(other).Next;
         linkedPhase.Remove(other);
         linkedPhase.AddAfter(nextNode, other);
+
+        //페이즈 번호 스왑
         int tempNum = nextNode.Value.phaseNum;
         nextNode.Value.phaseNum = other.phaseNum;
         other.phaseNum = tempNum;
+
+        //페이즈 RectTransform 정렬
         foreach (PhaseElement phase in linkedPhase)
         {
             phase.gameObject.transform.SetParent(null);
             phase.gameObject.transform.SetParent(phaseRect);
         }
+
+        //페이즈 추가버튼 정렬
         ReplaceAddBTN();
     }
     private IEnumerator ScrollBarCtrl()
