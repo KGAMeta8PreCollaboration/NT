9e294a75712cb45fc92275c03ca184247ccf2f0a
l97038789 <l97038789@gmail.com>
2025-03-07 10:18:07 +0900

    [FEAT] : 콜라이더 기반 판정 구현

--- ProjectNT/Assets/03.Code/Scripts/JudgementSystem/PlayerController.cs
+++ ProjectNT/Assets/03.Code/Scripts/JudgementSystem/PlayerController.cs
@@ -5,28 +5,35 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-	[SerializeField] private JudgementSystem _judgementSystem;
+    [SerializeField] private JudgementSystem[] _judgementSystems;
 
-	private NoteManager noteManager;
-	[SerializeField] private Woofer[] _woofers;
+    private NoteManager noteManager;
 
-	private void Start()
-	{
-		noteManager = FindObjectOfType<NoteManager>();
-	}
+    private void Start()
+    {
+        noteManager = FindObjectOfType<NoteManager>();
+    }
 
-	public void Create(InputAction.CallbackContext context)
-	{
-		if (context.performed)
-		{
-			noteManager.CreateNote(int.Parse(context.control.name) - 1);
-		}
-	}
-	public void Hit(InputAction.CallbackContext context)
-	{
-		if (context.performed && context.control.name.Equals("space"))
-		{
-			_judgementSystem.CheckTiming();
-		}
-	}
+    public void Create(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            noteManager.CreateNote(int.Parse(context.control.name) - 1);
+        }
+    }
+    public void Hit(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            InputControl control = context.control;
+            int index = control.name.Equals("a") ? 0 :
+                control.name.Equals("s") ? 1 :
+                control.name.Equals("d") ? 2 :
+                control.name.Equals("f") ? 3 : -1;
+
+            if (index == -1) return;
+
+            _judgementSystems[index].CheckTiming();
+        }
+    }
 }
