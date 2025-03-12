95aec71b65e29dfe73518dd489881b75c8bfc124
LEESEOHYEONG1031 <champ36912@naver.com>
2025-03-12 13:59:33 +0900

    [STYLE] : 알람 팝업 닫기 버튼 text 수정할 수 있도록 코드 추가

--- ProjectNT/Assets/03.Code/Scripts/UI/Popup/AlarmPopup.cs
+++ ProjectNT/Assets/03.Code/Scripts/UI/Popup/AlarmPopup.cs
@@ -8,11 +8,13 @@ using Game;
 
 public class AlarmPopup : Popup
 {
-    [SerializeField] private TextMeshPro _contentText;
+    [SerializeField] private TextMeshProUGUI _contentText;
+    [SerializeField] private TextMeshProUGUI _closeText;
 
-    public void SetPopup(string content, Action closeAction = null)
+    public void SetPopup(string content ,string closeText,Action closeAction = null)
     {
         _contentText.text = content;
+        _closeText.text = closeText;
         this.closeAction = closeAction;
     }
 }
