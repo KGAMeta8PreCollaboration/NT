43cdcfd4a83395ff1208e0bf82854f03d03961ba
LEESEOHYEONG1031 <champ36912@naver.com>
2025-03-26 10:20:41 +0900

    [FEAT] : 팝업 매니저 OpenPopup 기능 오버로딩

--- ProjectNT/Assets/03.Code/Scripts/UI/Popup/PopupManager.cs
+++ ProjectNT/Assets/03.Code/Scripts/UI/Popup/PopupManager.cs
@@ -28,13 +28,14 @@ public class PopupManager : MonoBehaviour
 
         return foundPopup;
     }
-
-    public void OpenPopup(Popup popup)
+    public T OpenPopup<T>(T popup) where T : Popup
     {
         if (popupList.Contains(popup))
         {
             popup.gameObject.SetActive(true);
+            return popup;
         }
+        return null;
     }
 
 
