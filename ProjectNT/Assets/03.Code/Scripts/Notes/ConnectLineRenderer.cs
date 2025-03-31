f20170a9e1b567ba04030393ac8f56e6d4e0d16b
LEESEOHYEONG1031 <champ36912@naver.com>
2025-03-31 10:58:05 +0900

    [STYLE] : 롱노트 Hold 시 라인렌더러 안과 바깥 모두 위치 안맞던거 수정

--- ProjectNT/Assets/03.Code/Scripts/Notes/ConnectLineRenderer.cs
+++ ProjectNT/Assets/03.Code/Scripts/Notes/ConnectLineRenderer.cs
@@ -7,8 +7,9 @@ using UnityEngine;
 
 public class ConnectLineRenderer : MonoBehaviour
 {
-    public Transform start;
+    public Transform startNote;
     public Transform endNote;
+    public Transform startPoint;
     public Transform endPoint;
     public Transform startRenderer;
     public LineRenderer lineRenderer;
@@ -20,23 +21,23 @@ public class ConnectLineRenderer : MonoBehaviour
 
     public void Init(float distance, Transform target)
     {
-        Vector3 railDirection = (start.position - target.position).normalized;
+        Vector3 railDirection = (startNote.position - target.position).normalized;
 
-        endNote.localPosition = start.localPosition + (railDirection * distance);
+        endNote.localPosition = startNote.localPosition + (railDirection * distance);
         //end.localPosition = start.localPosition + new Vector3(distance, 0, 0);
 
         lineRenderer.positionCount = 2;
         lineRenderer.useWorldSpace = true;
         lineRenderer.alignment = LineAlignment.TransformZ;
 
-        float z = (endNote.localPosition.z - start.localPosition.z);
+        float z = (endNote.localPosition.z - startNote.localPosition.z);
 
         // print($"롱노트 startPos: {start.localPosition.z}, endPos: {end.localPosition.z}");
         // print($"롱노트 startPos와 endPos의 차이: {z}");
         boxCollider.size = new Vector3(0.4f, 1, Mathf.Abs(z));
         boxCollider.center = new Vector3(0, 0, (z / 2));
 
-        _origin = start;
+        _origin = startPoint;
         _target = target;
 
         leftLR.Init(target);
@@ -45,13 +46,13 @@ public class ConnectLineRenderer : MonoBehaviour
 
     void Update()
     {
-        lineRenderer.SetPosition(0, start.position);
+        lineRenderer.SetPosition(0, startPoint.position);
         lineRenderer.SetPosition(1, endPoint.position);
     }
 
     public void Hold()
     {
-        start = _target;
+        startPoint = _target;
         leftLR.Hold();
         rightLR.Hold();
     }
@@ -60,7 +61,7 @@ public class ConnectLineRenderer : MonoBehaviour
     {
         //start = _origin;
         startRenderer.position = _target.position;
-        start = _origin;
+        startPoint = _origin;
 
         leftLR.Release();
         rightLR.Release();
