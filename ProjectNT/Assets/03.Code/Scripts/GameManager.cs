d3ad2db90ef24ba026ff2650bf8c1f1cae0efafa
LEESEOHYEONG1031 <champ36912@naver.com>
2025-03-31 17:18:18 +0900

    [WIP] : 멀티게임에서 로비로 나가기 기능 구현 중

--- ProjectNT/Assets/03.Code/Scripts/GameManager.cs
+++ ProjectNT/Assets/03.Code/Scripts/GameManager.cs
@@ -1,124 +1,120 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
-	public double delayTime = 2.0;
-	public Action OnGameEnd; 
-	
-	public NoteManager[] noteManagers;
-	public NoteGenerator[] noteGenerators;
-	private ResultPanel _resultPanel;
-	public bool skipLobby; //로비씬 없이 바로 게임 스타트 하는 개발용 변수.
+    public double delayTime = 2.0;
+    public Action OnGameEnd;
+    public Action OnGoToLobby;
 
-	[Header("게임 씬 이름")]
-	public string gameSceneName = "YKD_GameScene";
-	
-	
-	// TODO : 프로토타입용 임시 UI, 나중에 UIManager든 뭐든 뺄것
-	[SerializeField] private GameObject endGameMenuPanel;
-	List<LoadedNoteData> loadedNoteDatas = new List<LoadedNoteData>();
-	
-	private void Start()
-	{
-		print( "경로 : " + Application.persistentDataPath);
-		// GameSceneInit();
-		if (skipLobby)
-		{
-			GameSceneInit();
-			noteGenerators[0].Init();
-		}
-		SceneManager.sceneLoaded += OnSceneLoaded;
-	}
-	
-	private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-	{
-		print("Scene Loaded : " + scene.name);
-		if (scene.name == gameSceneName)
-		{
-			print("프로토타입 씬");
-			GameSceneInit();
-			noteGenerators[0].Init(loadedNoteDatas);
-		}
-	}
-	private ProjectToLoadedData _projectToLoadedData;
+    private NoteManager[] _noteManager;
+    private NoteGenerator[] _noteGenerator;
+    private GamePhotonManager _gamePhotonManager;
+    //private ResultPanel _resultPanel;
 
-	public void SingleGameStart(Difficulty difficulty, BeatMapData beatMapData, string projectPath)
-	{
-		_projectToLoadedData = gameObject.AddComponent<ProjectToLoadedData>();
-		_projectToLoadedData.GetAudioClipsToProject(projectPath, AudioManager.Instance.SetAudioClips);
-		_projectToLoadedData.GetBgmAudioClip(projectPath, beatMapData.songData.songName, AudioManager.Instance.SetBackgroundMusic);
-		SceneManager.LoadScene(gameSceneName);
-		loadedNoteDatas = _projectToLoadedData.BeatMapDataToLoadedNoteData(beatMapData);
-	}
-	
-	private IEnumerator StartCo()
-	{
-		yield return new WaitForSeconds(5f);
+    //// TODO : 프로토타입용 임시 UI, 나중에 UIManager든 뭐든 뺄것
+    //[SerializeField] private GameObject endGameMenuPanel;
 
-		GameStart();
-		StartCoroutine(CheckGameEndCoroutine());
-	}
 
-	private void GameSceneInit()
-	{
-		noteManagers = FindObjectsOfType<NoteManager>();
-		noteGenerators = FindObjectsOfType<NoteGenerator>();
-		_resultPanel = FindObjectOfType<ResultPanel>(true);
-		_resultPanel?.gameObject.SetActive(false);
-		endGameMenuPanel = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(obj => obj.name == "EndGameMenuPanel");
-		if (endGameMenuPanel == null)
-		{
-			Debug.LogError("EndGameMenuPanel not found in the scene.");
-		}
-		StopCoroutine(StartCo());
-		StartCoroutine(StartCo());
-	}
+    public BeatMapData beatMapData;
 
-	// TODO: 프로토타입 임시
+    [Header("로비없이 게임 시작하려면 체크")]
+    public bool skipLobby; //로비씬 없이 바로 게임 스타트 하는 개발용 변수.
+
+    private void Start()
+    {
+        print("경로 : " + Application.persistentDataPath);
+        if (skipLobby) GameSceneInit();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        print("Scene Loaded : " + scene.name);
+        if (scene.name == "GameScene" || scene.name == "LSH_MultiGame")
+        {
+            //print("프로토타입 씬");
+            GameSceneInit();
+
+            // 특정 씬이 로드될 때 수행할 행동들
+        }
+    }
+
+    public void SingleGameStart(Difficulty difficulty, BeatMapData beatMapData)
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void MultiGameStart(Difficulty difficulty, BeatMapData beatMapData)
+    {
+        SceneManager.LoadScene("LSH_MultiGame");
+        OnGoToLobby += () => _gamePhotonManager.LeaveRoom();
+    }
+
+    private void GameSceneInit()
+    {
+        _noteManager = FindObjectsOfType<NoteManager>();
+        _noteGenerator = FindObjectsOfType<NoteGenerator>();
+        _gamePhotonManager = FindObjectOfType<GamePhotonManager>();
+        //_resultPanel = FindObjectOfType<ResultPanel>(true);
+        //_resultPanel?.gameObject.SetActive(false);
+        //endGameMenuPanel = Resources.FindObjectsOfTypeAll<GameObject>().FirstOrDefault(obj => obj.name == "EndGameMenuPanel");
+        StopCoroutine(StartCoroutine());
+        StartCoroutine(StartCoroutine());
+    }
+
+    private IEnumerator StartCoroutine()
+    {
+        FindObjectOfType<GameSceneMove>()?.GameSceneMoveAndLightStart();
+        yield return new WaitForSeconds(5f);
+        GameStart();
+        StartCoroutine(CheckGameEndCoroutine());
+    }
+
+    // TODO: 프로토타입 임시
     public void GameStart()
-	{
-		print("게임매니저 게임스타트");
-		AudioManager.Instance.StartBGM(delayTime);
-		FindObjectOfType<GameSceneMove>()?.GameSceneMoveAndLightStart();
-	}
-	
-	public void GoToLobby()
-	{
-		SceneManager.LoadScene("Prototype_Lobby");
-	}
-	
-	public void GameEnd()
-	{
-		print("Game End");
-		_resultPanel?.gameObject.SetActive(true);
-		endGameMenuPanel?.SetActive(true);
-		OnGameEnd?.Invoke();
-	}
-	
-	public bool CheckGameEnd()
-	{
-		return !noteManagers.Any(item => item.notes.Count > 0)
-		       && noteGenerators.All(item => item.IsAllGenerated());
-	}
-	
-	private IEnumerator CheckGameEndCoroutine()
-	{
-		while (true)
-		{
-			if (CheckGameEnd())
-			{
-				print("CheckGameEndCoroutine");
+    {
+        AudioManager.Instance.StartBGM(delayTime);
+    }
 
-				GameEnd();
-				yield break;
-			}
-			yield return null;
-		}
-	}
+    public void GoToLobby()
+    {
+        OnGoToLobby?.Invoke();
+        OnGoToLobby = null;
+        SceneManager.LoadScene("Prototype_Lobby");
+    }
+
+    public void GameEnd()
+    {
+        print("Game End");
+        //_resultPanel?.gameObject.SetActive(true);
+        //endGameMenuPanel?.SetActive(true);
+        OnGameEnd?.Invoke();
+        OnGameEnd = null;
+    }
+
+    public bool CheckGameEnd()
+    {
+        return !_noteManager.Any(item => item.notes.Count > 0)
+               && _noteGenerator.All(item => item.IsAllGenerated());
+    }
+
+    private IEnumerator CheckGameEndCoroutine()
+    {
+        while (true)
+        {
+            //print("CheckGameEndCoroutine");
+            if (CheckGameEnd())
+            {
+                GameEnd();
+                yield break;
+            }
+            yield return null;
+        }
+    }
 
 }
