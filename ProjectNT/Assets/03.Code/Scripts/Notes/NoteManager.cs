21580a19c4866bc2b9cb88c0b4893650d0b66ada
yugd1211 <yugd1211@gmail.com>
2025-04-09 13:25:51 +0900

    [FEAT] : 키음 시스템 변경
    노트 키음 할당을 노트가 삭제되면 다음 노트의 키음을 우퍼에 할당하는 방식에서 다음 노트와의 거리를 계산하여 20%거리에는 이전 노트에 할당된 키음소리가 날수 있게끔 변경
    
    1. NoteAudioScheduler : 해당 레일의 노트들을 정렬하여 정박 DSPTime을 기준으로 다음 노트와의 시간을 계산하여 노트와 관계없이 특정 시간이 되면 우퍼에 키음을 할당

--- ProjectNT/Assets/03.Code/Scripts/Notes/NoteManager.cs
+++ ProjectNT/Assets/03.Code/Scripts/Notes/NoteManager.cs
@@ -12,6 +12,15 @@ public class NoteManager : MonoBehaviour
 
     public List<Note> notes { get; private set; } = new List<Note>();
 
+
+    private NoteGenerator _noteGenerator;
+
+
+    private void Awake()
+    {
+        _noteGenerator = GetComponent<NoteGenerator>();
+    }
+
     public void CreateNoteFromData(LoadedNoteData noteData)
     {
         NoteSpawnData noteSpawnData = null;
@@ -30,6 +39,37 @@ public class NoteManager : MonoBehaviour
             noteRails[noteData.railIndex].SpawnNote(AddNote, RemoveNote, noteSpawnData);
     }
 
+    public void AssignNotesToSchedulers(double startDspTime)
+    {
+        List<LoadedNoteData>[] railNotes = new List<LoadedNoteData>[4]
+        {
+            new List<LoadedNoteData>(),
+            new List<LoadedNoteData>(),
+            new List<LoadedNoteData>(),
+            new List<LoadedNoteData>(),
+        };
+        foreach (LoadedNoteData noteData in _noteGenerator.loadedNotes)
+        {
+            LoadedNoteData noteDataCopy = new LoadedNoteData(noteData);
+            noteDataCopy.time += startDspTime;
+            if (noteData.noteType == NoteType.Long)
+                noteDataCopy.endTime += startDspTime;
+            if (noteData.railIndex >= 0 && noteData.railIndex < railNotes.Length)
+                railNotes[noteData.railIndex].Add(noteDataCopy);
+        }
+
+        for (int i = 0; i < noteRails.Count; i++)
+        {
+            NoteRail rail = noteRails[i];
+            NoteAudioScheduler scheduler = rail.GetComponent<NoteAudioScheduler>();
+            if (scheduler != null)
+            {
+                scheduler.Init(railNotes[i]);
+            }
+        }
+    }
+    
+    
     // private void OnNoteHit(Note note)
     // {
     //     if (note.judgementType == JudgementType.Bad)
