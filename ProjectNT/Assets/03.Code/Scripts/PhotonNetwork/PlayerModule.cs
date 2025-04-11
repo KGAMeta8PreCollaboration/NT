3a6fa5851c72ff454493d81b5521ff312c30d712
LEESEOHYEONG1031 <champ36912@naver.com>
2025-04-11 09:28:45 +0900

    [WIP] : 상대방 노트 판정에 대한 처리 동기화 중

--- ProjectNT/Assets/03.Code/Scripts/PhotonNetwork/PlayerModule.cs
+++ ProjectNT/Assets/03.Code/Scripts/PhotonNetwork/PlayerModule.cs
@@ -1,4 +1,5 @@
 using Photon.Pun;
+using Photon.Pun.Demo.Cockpit;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,6 +17,7 @@ public class PlayerModule : MonoBehaviour
     public NoteManager NoteManager { get; private set; }
     public NoteGenerator NoteGenerator { get; private set; }
     public ScoreManager ScoreManager { get; private set; }
+    public Woofer[] woofers { get; private set; }
 
     private void Awake()
     {
@@ -23,6 +25,12 @@ public class PlayerModule : MonoBehaviour
         NoteManager = TransformUtil.FindDeepChildComponent<NoteManager>(transform, "NoteManager");
         NoteGenerator = TransformUtil.FindDeepChildComponent<NoteGenerator>(transform, "NoteManager");
         ScoreManager = TransformUtil.FindDeepChildComponent<ScoreManager>(transform, "NoteManager");
+
+        woofers = new Woofer[4];
+        for (int i = 0; i < 4; i++)
+        {
+            woofers[i] = TransformUtil.FindDeepChildComponent<Woofer>(NoteManager.noteRails[i].transform, "Woofer");
+        }
     }
 
     public void SetPlayerModuleData(List<LoadedNoteData> playerSongData)
