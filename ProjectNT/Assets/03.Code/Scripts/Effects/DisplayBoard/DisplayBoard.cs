91c81bb61b4d066e8b27a5ca7da93c0016f2e92d
SIRO\gmlwo <gmlwo990518@naver.com>
2025-04-24 14:55:30 +0900

    [FEAT] EffectManager에 Action 등록

--- ProjectNT/Assets/03.Code/Scripts/Effects/DisplayBoard/DisplayBoard.cs
+++ ProjectNT/Assets/03.Code/Scripts/Effects/DisplayBoard/DisplayBoard.cs
@@ -6,12 +6,14 @@ public class DisplayBoard : MonoBehaviour
 {
     [SerializeField] private Texture2D[] gifImage;
     [SerializeField] private Texture2D originalTexture;
+    [SerializeField] private Texture2D blackTexture;
     [SerializeField] private float delay = 0.1f;
     [SerializeField] private float duration = 2f;
 
     private Material _targetMaterial;
     private int _currentIndex = 0;
-    private Coroutine _displayCoroutine;
+    private Coroutine _changeDisplayCoroutine;
+    private Coroutine _turnOnDisplayCoroutine;
     private DisplayBoardHandler _displayBoardHandler;
 
     private void Awake()
@@ -21,8 +23,13 @@ public class DisplayBoard : MonoBehaviour
 
     private void Start()
     {
-        _displayBoardHandler.displayBoardCallback += ChangeDisplayBoard;
+        _displayBoardHandler.changeDisplayBoardCallback += ChangeDisplayBoard;
+        _displayBoardHandler.turnOnDisplayBoardCallback += TurnOnDisplayBoard;
+        _displayBoardHandler.setDisplayOriginalCallback += SetDisplayToOriginal;
         _targetMaterial = GetComponent<Renderer>().material;
+
+        //시작할때는 검정색 화면인 상태
+        SetDisplayToBlack();
     }
 
     //private void Update()
@@ -36,17 +43,48 @@ public class DisplayBoard : MonoBehaviour
     //    }
     //}
 
-    private void ChangeDisplayBoard()
+    //초기 상태는 검정색 화면이여야 함
+    private void SetDisplayToBlack()
     {
-        if (_displayCoroutine != null)
-        {
-            StopCoroutine(_displayCoroutine);
-        }
-
-        _displayCoroutine = StartCoroutine(HandleDisplayBoard());
+        _targetMaterial.SetTexture("_MainTexture", blackTexture);
     }
 
-    private IEnumerator HandleDisplayBoard()
+    private void SetDisplayToOriginal()
+    {
+        _targetMaterial.SetTexture("_MainTexture", originalTexture);
+    }
+
+    //디스플레이 켜는 함수
+    private void TurnOnDisplayBoard()
+    {
+        if (_turnOnDisplayCoroutine != null)
+        {
+            StopCoroutine(_turnOnDisplayCoroutine);
+        }
+
+        _turnOnDisplayCoroutine = StartCoroutine(TurnOnDisplayBoardCoroutine());
+    }
+
+    private IEnumerator TurnOnDisplayBoardCoroutine()
+    {
+        _targetMaterial.SetTexture("_MainTexture", originalTexture);
+        yield return new WaitForSeconds(duration);
+        _targetMaterial.SetTexture("_MainTexture", blackTexture);
+    }
+
+
+    //이퀄라이저로 바꾸는 함수
+    private void ChangeDisplayBoard()
+    {
+        if (_changeDisplayCoroutine != null)
+        {
+            StopCoroutine(_changeDisplayCoroutine);
+        }
+
+        _changeDisplayCoroutine = StartCoroutine(ChangeDisplayBoardCoroutine());
+    }
+
+    private IEnumerator ChangeDisplayBoardCoroutine()
     {
         float elapsed = 0f;
         int currentIndex = 0;
@@ -61,6 +99,6 @@ public class DisplayBoard : MonoBehaviour
         }
 
         _targetMaterial.SetTexture("_MainTexture", originalTexture);
-        _displayCoroutine = null;
+        _changeDisplayCoroutine = null;
     }
 }
--- ProjectNT/Assets/03.Code/Scripts/Effects/DisplayBoard/DisplayBoardHandler.cs
+++ ProjectNT/Assets/03.Code/Scripts/Effects/DisplayBoard/DisplayBoardHandler.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 
 public class DisplayBoardHandler : MonoBehaviour
 {
-    public Action displayBoardCallback;
+    public Action changeDisplayBoardCallback;
+    public Action turnOnDisplayBoardCallback;
+    public Action setDisplayOriginalCallback;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.K))
         {
-            displayBoardCallback?.Invoke();
+            changeDisplayBoardCallback?.Invoke();
         }   
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            turnOnDisplayBoardCallback?.Invoke();
+        }
+    }
+
+    public void ChangeDisplayBoard()
+    {
+        changeDisplayBoardCallback?.Invoke();
+    }
+
+    public void TurnOnDisplayBoard()
+    {
+        turnOnDisplayBoardCallback?.Invoke();
+    }
+
+    public void SetDisplayOriginal()
+    {
+        setDisplayOriginalCallback?.Invoke();
     }
 }
