3d97a3dda2aa0feb1ea693b47552ef54d575ec9f
SIRO\gmlwo <gmlwo990518@naver.com>
2025-04-25 16:18:36 +0900

    [REFACTOR] 2페이즈에 레이저를 10콤보로, 광고 이퀄 변경을 20콤보로 변경. 3페이즈에는 이퀄라이저 고정으로 변경

--- ProjectNT/Assets/03.Code/Scripts/Effects/DisplayBoard/DisplayBoard.cs
+++ ProjectNT/Assets/03.Code/Scripts/Effects/DisplayBoard/DisplayBoard.cs
@@ -24,27 +24,44 @@ public class DisplayBoard : MonoBehaviour
 
     private void Start()
     {
+        //이퀄로 바꾼 후 다시 오리지널
         _displayBoardHandler.changeDisplayBoardCallback += ChangeDisplayBoard;
-        _displayBoardHandler.turnOnDisplayBoardCallback += TurnOnDisplayBoard;
-        _displayBoardHandler.setDisplayOriginalCallback += SetDisplayToOriginal;
+        //오리지널로 바꾼 후 다시 검은 화면
+        //_displayBoardHandler.turnOnDisplayBoardCallback += TurnOnDisplayBoard;
+        //오리지널로 켜놓음
+        //_displayBoardHandler.setDisplayOriginalCallback += SetDisplayToOriginal;
+        //이퀄라이저로 켜놓음
+        _displayBoardHandler.setDisplayEqualizerCallback += SetDisplayBoardToEqualizer;
         _targetMaterial = GetComponent<Renderer>().material;
 
-        //시작할때는 검정색 화면인 상태
-        SetDisplayToBlack();
+        SetDisplayToOriginal();
     }
 
-    //private void Update()
-    //{
-    //    timer += Time.deltaTime;
-    //    if (timer >= delay)
-    //    {
-    //        _currentIndex = (_currentIndex + 1) % gifImage.Length;
-    //        _targetMaterial.SetTexture("_MainTexture", gifImage[_currentIndex]);
-    //        timer = 0f;
-    //    }
-    //}
+    //테스트 할때 열어보시오
+    //float elapsed = 0;
+    //bool tmp = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SetDisplayBoardToEqualizer();
+            //    tmp = !tmp;
+            //}
+        }
 
-    //초기 상태는 검정색 화면이여야 함
+        //if (tmp)
+        //{
+        //    elapsed += Time.deltaTime;
+        //    if (elapsed >= delay)
+        //    {
+        //        _currentIndex = (_currentIndex + 1) % gifImage.Length;
+        //        _targetMaterial.SetTexture("_MainTexture", gifImage[_currentIndex]);
+        //        elapsed = 0f;
+        //    }
+        //}
+    }
+
+    //초기 상태는 검정색 화면이여야 함 -> 변경됨
     private void SetDisplayToBlack()
     {
         _targetMaterial.SetTexture("_MainTexture", blackTexture);
@@ -105,5 +122,27 @@ public class DisplayBoard : MonoBehaviour
 
         _targetMaterial.SetTexture("_MainTexture", originalTexture);
         _changeDisplayCoroutine = null;
+    }
+
+    private void SetDisplayBoardToEqualizer()
+    {
+        StartCoroutine(SetDisplayBoardToEqualizerCoroutine());
+    }
+
+    private IEnumerator SetDisplayBoardToEqualizerCoroutine()
+    {
+        int currentIndex = 0;
+        float timer = 0f;
+        while (true)
+        {
+            timer += Time.deltaTime;
+            if (timer >= delay)
+            {
+                _targetMaterial.SetTexture("_MainTexture", gifImage[currentIndex]);
+                currentIndex = (currentIndex +1) % gifImage.Length;
+                timer = 0f;
+            }
+            yield return null;
+        }
     }
 }
